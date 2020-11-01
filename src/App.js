af1d206cf3849f604b0add354bfd447679e89eb1
dutianze <dutianzer@gmail.com>
2020-11-01 11:57:00 +0800

    feat: add shapshot and add form in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+        <header>
+            <h1>Ed's Todo List</h1>
+        </header>
+        <form>
+            <input type="text" className="todo-input"/>
+            <button className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
     </div>
   );
 }
